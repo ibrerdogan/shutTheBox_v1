4a1e481ee3f7eecaeada64551219d9143ee25ace
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2023-11-11 09:28:33 +0300

    Initial Commit

---
+++ shutTheBox/shutTheBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  shutTheBoxApp.swift
+//  shutTheBox
+//
+//  Created by İbrahim Erdogan on 11.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct shutTheBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
